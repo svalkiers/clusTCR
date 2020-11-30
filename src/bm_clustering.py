b6bda91913abb06b30471f8e62c62c6efcc83ec0
Sebastiaan Valkiers <sebastiaanvalkiers97@gmail.com>
2020-11-30 11:32:40 +0100

    MCL parameter scanning

--- src/bm_clustering.py
+++ src/bm_clustering.py
@@ -13,8 +13,8 @@ import numpy as np
 from networkTCR import clustering
 from networkTCR import metrics
 
-bm_file = "../data/vdjdb_trb.tsv"
-bm_output = "../results/bm_metrics.tsv"
+bm_file = "../data/vdjdb_trb.tsv" # input
+bm_output = "../results/bm_metrics.tsv" # output
 
 def read_vdjdb(vdjdb_file, q_score = 0):
     '''
@@ -26,22 +26,38 @@ def read_vdjdb(vdjdb_file, q_score = 0):
     
     return vdjdb[["CDR3", "Epitope"]]
 
-# Read input file
-bm = read_vdjdb(bm_file)
-cdr3 = set(bm["CDR3"])
+# Generate list of hyperparameter pairs to test
+params = []
+param_1 = np.round(np.arange(1.1,3.1,0.1),1)
+param_2 = np.round(np.arange(2,6,1),0)
+for i in param_1:
+    for j in param_2:
+        params.append([i, j])
 
-# Perform clustering procedure using networkTCR
-Clust = clustering(cdr3)
-edges = Clust.createNetwork()
-nodes = Clust.network_clustering(edges)
+# Scan hyperparameter space
+results = []
+c = 0
+for pair in params:
 
-# Calculate benchmarking metrics
-Benchmark = metrics(nodes, bm)
-retention = Benchmark.retention()
-purity = Benchmark.purity()
-results = [1.2, 2, retention, purity["Regular"], purity["Baseline"]]
+    # Read input file
+    bm = read_vdjdb(bm_file)
+    cdr3 = set(bm["CDR3"])
+    
+    # Perform clustering procedure using networkTCR
+    Clust = clustering(cdr3)
+    edges = Clust.createNetwork()
+    nodes = Clust.network_clustering(edges, mcl_hyper=pair)
+    
+    # Calculate benchmarking metrics
+    Benchmark = metrics(nodes, bm)
+    retention = Benchmark.retention()
+    purity = Benchmark.purity()
+    results.append([pair[0], pair[1], retention, purity["Regular"], purity["Baseline"]])
+    
+    c += 1
+    print(c)
 
 # Write output to file
 colnames = ["Inflation", "Expansion", "Retention", "Purity_r", "Purity_b"]
-output = pd.DataFrame(np.array(results).reshape(-1,len(results)), columns=colnames)
+output = pd.DataFrame(np.array(results), columns=colnames)
 output.to_csv(bm_output, index=False, sep="\t")
