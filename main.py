267d49fe8b9cf56d97d45fe7d7092a2082ccd93c
Max Van Houcke <max.vanhoucke@student.uantwerpen.be>
2021-02-25 13:41:53 +0100

    Update time plot

--- main.py
+++ main.py
@@ -46,23 +46,24 @@ def plot_time():
     x = []
 
     ismart_y, gliph2_y, ismart_clustcr_y, ismart_par_clustcr_y, gliph2_clustcr_y, gliph2_par_clustcr_y, mcl_y = [], [], [], [], [], [], []
-    for q in [0, 1, 2]:
-        epitopes = datasets.vdjdb_epitopes_small(q=q)
-        data_gliph2 = datasets.vdjdb_gliph2_small(q=q)
-        data = datasets.vdjdb_cdr3_small(q=q)
+    for q in [500, 10000, 100000]:
+        # data_gliph2 = datasets.vdjdb_gliph2_small(q=q)
+        # data = datasets.vdjdb_cdr3_small(q=q)
+        data_gliph2 = datasets.metarepertoire('/home/max/Downloads/emerson-2017-natgen', 'immuneaccess', out_format='GLIPH2', n_sequences=q)
+        data = datasets.metarepertoire('/home/max/Downloads/emerson-2017-natgen', 'immuneaccess', n_sequences=q)
         x.append(len(data))
 
         ismart_clustcr_y.append(
-            timer(lambda: Clustering(second_step='ismart').fit(data).metrics(epitopes)))
+            timer(lambda: Clustering(second_step='ismart').fit(data)))
         gliph2_clustcr_y.append(
-            timer(lambda: Clustering(second_step='gliph2').fit(data_gliph2).metrics(epitopes)))
+            timer(lambda: Clustering(second_step='gliph2').fit(data_gliph2)))
         ismart_par_clustcr_y.append(
-            timer(lambda: Clustering(second_step='ismart', n_cpus='all').fit(data).metrics(epitopes)))
+            timer(lambda: Clustering(second_step='ismart', n_cpus='all').fit(data)))
         gliph2_par_clustcr_y.append(
-            timer(lambda: Clustering(second_step='gliph2', n_cpus='all').fit(data_gliph2).metrics(epitopes)))
-        mcl_y.append(timer(lambda: Clustering(second_step='mcl').fit(data).metrics(epitopes)))
-        ismart_y.append(timer(lambda: ClusteringResult(ismart.iSMART(data)[0]).metrics(epitopes)))
-        gliph2_y.append(timer(lambda: ClusteringResult(gliph2.GLIPH2(data_gliph2)[0]).metrics(epitopes)))
+            timer(lambda: Clustering(second_step='gliph2', n_cpus='all').fit(data_gliph2)))
+        mcl_y.append(timer(lambda: Clustering(second_step='mcl').fit(data)))
+        ismart_y.append(timer(lambda: ClusteringResult(ismart.iSMART(data)[0])))
+        gliph2_y.append(timer(lambda: ClusteringResult(gliph2.GLIPH2(data_gliph2)[0])))
 
     plt.plot(x, ismart_y, label='iSMART', marker='o')
     plt.plot(x, gliph2_y, label='GLIPH2', marker='o')
