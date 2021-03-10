fc04f8342cd6d00540c937d2544278bfb3b3811e
Sebastiaan Valkiers <sebastiaan.valkiers@uantwerpen.be>
2021-03-10 07:21:45 +0100

    Parsing function for 10X data format

---
+++ clustcr/input/tenx.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def parse_10x(filename, out_format='CDR3', separator='\t'):
+    data = pd.read_csv(filename, sep=separator)
+    if out_format=='CDR3':
+        return pd.Series(data.Bcdr3.unique())
