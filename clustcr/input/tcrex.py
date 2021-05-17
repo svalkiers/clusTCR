76a66bb96cb5076e7157854717ba9b35c7e47627
Sebastiaan Valkiers <sebastiaan.valkiers@uantwerpen.be>
2021-05-17 14:49:28 +0200

    FIxed TCRex parsing issue

--- clustcr/input/tcrex.py
+++ clustcr/input/tcrex.py
@@ -4,7 +4,7 @@ def parse_tcrex(filename, out_format='CDR3', separator='\t'):
     data = pd.read_csv(filename, sep=separator)
     data.drop(columns=['TRBJ_gene'], inplace=True)
     if out_format.upper() == 'CDR3':
-        return data.CDR3_beta.unique()
+        return pd.Series(data.CDR3_beta.unique())
     elif out_format.upper() == 'GLIPH2':
         return data.rename(columns={'CDR3_beta': 'CDR3', 'TRBV_gene': 'V'})
     elif out_format.upper() == 'TCRDIST':
